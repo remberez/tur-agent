0243d9c47129b7bbba04a5312343bbfef9440151
Agasyan <agasyan222@gmail.com>
2025-05-02 23:23:32 +0400

    create admin page layout

---
+++ frontend/src/components/AdminPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div className="w-64 bg-gray-800 text-white h-full p-4">
+      <h2 className="text-2xl font-bold mb-8">Админка</h2>
+      <ul>
+        <li>
+          <Link to="/admin/cities" className="block p-2 hover:bg-gray-700">Города</Link>
+        </li>
+        <li>
+          <Link to="/admin/countries" className="block p-2 hover:bg-gray-700">Страны</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const AdminPage = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+
+      <div className="flex-1 p-4">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default AdminPage;
