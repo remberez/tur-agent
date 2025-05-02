9c030642f2977eb6dd299e9a22de25938559afa6
Agasyan <agasyan222@gmail.com>
2025-05-02 22:58:34 +0400

    add stuff check function

--- backend/permissions.py
+++ backend/permissions.py
@@ -1,9 +1,19 @@
 from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from models import Client, EmployeePositionEnum
 from auth import get_current_client
 
 
 async def admin_required(current_user: Client = Depends(get_current_client)):
     if not current_user.employee or current_user.employee.position != EmployeePositionEnum.admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Недостаточно прав")
+    return current_user
+
+
+async def staff_required(
+    current_user: Client = Depends(get_current_client),
+):
+    if not current_user.employee:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Недостаточно прав")
     return current_user
