3fc1f2b3eb45fce2e5b1c0d07c32ce264616ee3e
Agasyan <agasyan222@gmail.com>
2025-05-02 23:23:06 +0400

    create main page component

---
+++ frontend/src/components/MainPage.jsx
@@ -0,0 +1,16 @@
+import SearchForm from './SearchForm';
+import Testimonials from './Testimonials';
+import WhyChooseUs from './WhyChooseUs';
+
+const MainPage = () => {
+  
+    return (
+        <>
+            <SearchForm/>
+            <WhyChooseUs/>
+            <Testimonials/>
+        </>
+    )
+};
+
+export default MainPage;
