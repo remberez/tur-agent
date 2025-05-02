5d89ae3522b4b364932584a0b2377d9e7012b2a6
Agasyan <agasyan222@gmail.com>
2025-05-02 23:22:28 +0400

    create country service

---
+++ frontend/src/services/countryService.js
@@ -0,0 +1,23 @@
+import api from './api';
+
+export const countryService = {
+  async getCountries() {
+    const response = await api.get('/countries');
+    return response.data;
+  },
+
+  async createCountry(countryData) {
+    const response = await api.post('/countries', countryData);
+    return response.data;
+  },
+
+  async updateCountry(countryId, countryData) {
+    const response = await api.put(`/countries/${countryId}`, countryData);
+    return response.data;
+  },
+
+  async deleteCountry(countryId) {
+    const response = await api.delete(`/countries/${countryId}`);
+    return response.data;
+  },
+};
